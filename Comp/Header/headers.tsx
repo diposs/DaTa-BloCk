340e58329762d9f0f28e14b60cb5e8c068574583
diposs <onakunle.dipo@gmail.com>
2023-03-25 23:08:48 +0100

    Update headers.tsx

--- Comp/Header/headers.tsx
+++ Comp/Header/headers.tsx
@@ -80,7 +80,25 @@ export function Headings() {
     // let data = await db.collection('waitingRoom').create(['googled']);
     // let datas = await db.collection('waitingRoom').create(['BLUE']);
     // let datass = await db.collection('GlobalEpoch').create(['recents']);
-    const bll = await db.collection('CollectionName').record('0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c').call("addMember",[db.collection('User').record(publicKey!)]);
+    const bll = await db.collection('CollectionName').record('0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c').call("addMember",[{
+      "block_number": "15846571",
+      "block_timestamp": "2022-10-28T12:30:47.000Z",
+      "block_hash": "0x5bdb81ef57386daa6ad3b9893216db9b15bb5d88f9c5524fabb106eace4c69c6",
+      "transaction_hash": "0x0e6bbf44bcc56309ed01797dbcbda46a50689c65fa861efe1f3f8024e2a7e775",
+      "transaction_index": 105,
+      "log_index": 133,
+      "value": "5000000000000000",
+      "contract_type": "ERC721",
+      "transaction_type": "Single",
+      "token_address": "0x3f457a3fd454b711867670846e7cc525b249b5b6",
+      "token_id": "1312",
+      "from_address": "0xb3fde20382cb896fdcc7fb27838b8def47a0d8e9",
+      "to_address": "0xa8eda30479d3827a53fa2268af00d8effd368719",
+      "amount": "1",
+      "verified": 1,
+      "operator": null,
+      "possible_spam": false
+    }]);
     // const bll = await db.collection('DragTest').get();
     // const data = await db.collection('CollectionName').create(['googled']);
     // const datas = await db.collection('CollectionName').record(publicKey).call("addMember",[db.collection('User').record("0x56b0572f5e5e264400eb4087f1df8542793a9eccf49764c4e2f0b466b69e95bf3acf9a9987c9b80231b8efda620397236c8dd6ecec64caa7bfced0d18f06bb20")] );
