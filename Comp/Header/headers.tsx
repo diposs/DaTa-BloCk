6d110e3a12f0a66234b5c1215803332eae25f0e5
diposs <onakunle.dipo@gmail.com>
2023-03-25 21:01:03 +0100

    Update headers.tsx

--- Comp/Header/headers.tsx
+++ Comp/Header/headers.tsx
@@ -10,7 +10,7 @@ const db = new Polybase({
   defaultNamespace: 'pk/0x51010b04ec558e9782ae7fdc57280d00393e57346bbc8cc0252f28c15bf568bcfb056b79a34417fb5d9b589b7ad3d3eb0554e224ea5f7a51a8bcc2768493b611/dasiybook',
 })
 
-const auth = typeof window !== "undefined" ? new Auth() : null;
+const auth = new Auth();
 
 
 async function getPublicKey() {
