2f6ccc97eef2ecfc45e7832d55c7acbce4d80469
diposs <onakunle.dipo@gmail.com>
2023-03-26 16:19:24 +0100

    Update header.style.ts

--- Comp/Header/header.style.ts
+++ Comp/Header/header.style.ts
@@ -2,24 +2,15 @@ import { createStyles } from '@mantine/core';
 
 
 export default createStyles((theme) => ({
-    inner: {
-      height: '60px',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+    headerblock: {
+      [theme.fn.smallerThan('lg')]: {
+        display: 'none',
+      },
     },
-  
-    
   
     burger: {
       [theme.fn.largerThan('sm')]: {
         display: 'none',
       },
-    },
-  
-    overflowwrapeng:{
-	    [theme.fn.largerThan('xs')]: {
-		overflow-wrap: 'anywhere',
-	},
     },
   }));
